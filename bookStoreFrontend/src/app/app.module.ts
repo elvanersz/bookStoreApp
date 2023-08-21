577e6feb939a98e131bd6e9cb3708df77b85d437
Elvan Ersöz <elvanersoz@outlook.com>
2023-08-21 11:49:29 +0300

    components created

--- bookStoreFrontend/src/app/app.module.ts
+++ bookStoreFrontend/src/app/app.module.ts
@@ -4,10 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { LoginComponent } from './guest/login/login.component';
+import { RegisterComponent } from './guest/register/register.component';
+import { HomeComponent } from './guest/home/home.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { AdminComponent } from './admin/admin/admin.component';
+import { NotFoundComponent } from './error/not-found/not-found.component';
+import { UnauthorizedComponent } from './error/unauthorized/unauthorized.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProfileComponent,
+    AdminComponent,
+    NotFoundComponent,
+    UnauthorizedComponent
   ],
   imports: [
     BrowserModule,
